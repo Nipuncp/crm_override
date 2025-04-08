30045d2cbda6105e113e4414bb53e44a4a5c4744
Nipuncp <nipuncp123@gmail.com>
2025-04-08 08:57:52 +0000

    change dashboard iframe

--- frontend/src_override/pages/Video.vue
+++ frontend/src_override/pages/Video.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="grid h-full text-gray--600">
-        <iframe src="http://nura.localhost/insights/shared/dashboard/huc4b00p" width="100%" height="100%" frameborder="0"></iframe>    
+        <iframe src="https://nuracrm-test.onedesk.app/insights/shared/dashboard/e35j7h0e16" width="100%" height="100%" frameborder="0"></iframe>    
     </div>
 </template>
