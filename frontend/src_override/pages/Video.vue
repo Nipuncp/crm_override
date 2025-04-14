60c5243050c83d8dd4b4b728fecf654768cd705e
Nipuncp <nipuncp123@gmail.com>
2025-04-14 13:52:36 +0000

    Change url to live site

--- frontend/src_override/pages/Video.vue
+++ frontend/src_override/pages/Video.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="grid h-full text-gray--600">
-        <iframe src="https://nuracrm-test.onedesk.app/insights/shared/dashboard/e35j7h0e16" width="100%" height="100%" frameborder="0"></iframe>    
+      <iframe src="https://nuracrm.onedesk.app/insights/shared/dashboard/r5eee6i04e" width="100%" height="100%" frameborder="0"></iframe>    
     </div>
 </template>
